1b6a4329212bfd858ea000204190f284c1df252a
coderstephen <me@stephencoakley.com>
2015-01-07 18:33:22 -0600

    404 page styling

--- themes/stephencoakley/404.php
+++ themes/stephencoakley/404.php
@@ -1,90 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>
-      <?php 
-        $title= (isset($global['title'])) ? $global['title'] : $global['site.title'];
-        echo $global['site.name'] .' | '. $title;
-      ?>
-    </title>
-    <meta name="description" content="">
-    <meta name="author" content="<?php echo $global['author.name']; ?>">  
+    <head>
+        <meta charset="utf-8">
+        <title><?=(isset($global['title'])?$global['title'].' - ':'').$global['site.name']?></title>
+        <meta name="description" content="<?php echo $global['site.description']; ?>">
+        <meta name="keywords" content="Stephen Coakley, programming, web development, apps, php">
+        <meta name="author" content="<?php echo $global['author.name']; ?>">
+        <link href="/blog/feed" type="application/atom+xml" rel="alternate" title="Blog Feed">
 
-    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+        <link rel="icon" type="image/png" href="<?php echo $global['assets.prefix'];?>/images/favicon.128.png" sizes="128x128">
+        <link rel="icon" type="image/png" href="<?php echo $global['assets.prefix'];?>/images/favicon.64.png" sizes="64x64">
+        <link rel="icon" type="image/png" href="<?php echo $global['assets.prefix'];?>/images/favicon.48.png" sizes="48x48">
+        <link rel="icon" type="image/png" href="<?php echo $global['assets.prefix'];?>/images/favicon.32.png" sizes="32x32">
+        <link rel="shortcut icon" href="<?php echo $global['assets.prefix'];?>/favicon.ico">
+        <link rel="icon" type="image/png" href="<?php echo $global['assets.prefix'];?>/images/favicon.16.png" sizes="16x16">
+        <link rel="apple-touch-icon" href="<?php echo $global['assets.prefix'];?>/images/favicon.128.png">
+        <meta name="msapplication-TileColor" content="#D83434">
+        <meta name="msapplication-TileImage" content="<?php echo $global['assets.prefix'];?>/images/favicon.128.png">
 
-    <!-- Le styles -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
-    <link href="<?php echo $global['assets.prefix'];?>/themes/textpress/assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo $global['assets.prefix'];?>/themes/textpress/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
-    <link href="<?php echo $global['assets.prefix'];?>/themes/textpress/assets/css/main.css" rel="stylesheet">
+        <meta name="viewport" content="initial-scale=1">
+        <link rel="stylesheet" href="<?php echo $global['assets.prefix'];?>/css/style.css">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/github.min.css">
 
-    <?php if($global['google.analytics']){?>
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', '<?php echo $global['google.analytics']; ?>']);
-      _gaq.push(['_trackPageview']);
+        <!--[if lt IE 9]>
+        <script src="<?php echo $global['assets.prefix'];?>/js/html5shiv.min.js"></script>
+        <![endif]-->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js"></script>
+        <script>hljs.initHighlightingOnLoad();</script>
+    </head>
 
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-      $(function(){
-        $('.dropdown-toggle').dropdown()  
-      })
-  </script>
-  <?php }?>
-  </head>
+    <body id="top">
+        <header class="page-header">
+            <p class="center">
+                <a class="site-logo no-underline" href="/" rel="home">
+                    <img src="/images/logo-white.svg">
+                    <span class="site-logo-text">Stephen Coakley</span>
+                </a>
+            </p>
 
-  <body>
-    <div style="background:#0a777d; width:100%; height:5px;"></div>
-    <div class="header">
-      <div class="container-narrow">
-        <div class="masthead text-center">
-          <h1 class="muted">TextPress</h1>
-          <ul class="nav nav-pills">
-            <li><a href="/">Home</a></li>
-            <li class="dropdown">
-              <a class="dropdown-toggle" id="drop5" role="button" data-toggle="dropdown" href="#">Categories <b class="caret"></b></b></a>
-              <ul id="menu3" class="dropdown-menu" role="menu" aria-labelledby="drop5">
-                <?php
-                if(isset($global['categories'])){
-                  foreach ($global['categories'] as $slug => $category) {
-                    echo '<li><a tabindex="-1" href="/category/'.$slug.'">'. $category .'</a></li>';
-                  }
-                }
-                ?>
-              </ul>
-            </li>
-            <li><a href="/archives">Archives</a></li>
-            <li><a href="https://github.com/shameerc/TextPress" target="_blank">Source</a></li>
-            <li><a href="/about">About</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="container-narrow">
-      <div class="row-fluid">
-        <div class="span10 offset1">
-          <h1>404, Not Found.</h1>
-          <p>Sorry, the page you are looking for is not found.</p>
-        </div>
-      </div>
-    </div>
-    <hr>
-    <footer id="site-footer">
-        <p>Powered by TextPress. &copy; <a href="http://blog.shameerc.com" target="_blank">Shameer C</a> 2013.</p>
-    </footer>
+            <p class="bio-text">Disciple of Christ, software developer, PHP enthusiast, techie, guitarist and musician, amateur photographer, lover of literature.</p>
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    
-    <script type="text/javascript" src="<?php echo $global['assets.prefix'];?>/themes/textpress/assets/js/bootstrap.min.js"></script>
-  </body>
+            <nav>
+                <a class="button" href="/">Home</a>
+                <a class="button" href="/portfolio">Portfolio</a>
+                <a class="button" href="/blog">Blog</a>
+            </nav>
+        </header>
+
+        <main role="main">
+            <div class="content">
+                <h1>This is not the page you are looking for.</h1>
+
+                <p><img src="/images/yoda.png"></p>
+
+                <p class="big">Found, the page you requested, was not. Perhaps, try another link at the top or left you will, hmm? Since came so far, you have, enjoy a random ASCII string, you will.</p>
+
+                <pre><code class="json" style="word-wrap:break-word;">"<?php
+                    for ($i = 0; $i < 1000; $i++) {
+                        $char = chr(rand(32, 126));
+                        if ($char !== '<') echo $char;
+                    }
+                ?>"</code></pre>
+
+                <p>Those Insecticons must be eating my hard drives again...</p>
+            </div>
+
+            <a class="button top-link fa fa-arrow-up" href="#top"></a>
+
+            <footer class="page-footer">
+                <p class="page-copyright">All content and design copyright &copy; Stephen Coakley.</p>
+                <p>Hosted by my lovely provider <a href="http://hostek.com">Hostek.com</a>.</p>
+            </footer>
+        </main>
+
+        <script>
+            var _paq=_paq||[];_paq.push(["trackPageView"]);_paq.push(["enableLinkTracking"]);(function(){var e=("https:"==document.location.protocol?"https":"http")+"://analytics.stephencoakley.com/";_paq.push(["setTrackerUrl",e+"piwik.php"]);_paq.push(["setSiteId","1"]);var t=document,n=t.createElement("script"),r=t.getElementsByTagName("script")[0];n.type="text/javascript";n.defer=true;n.async=true;n.src=e+"piwik.js";r.parentNode.insertBefore(n,r)})();
+        </script>
+    </body>
 </html>
