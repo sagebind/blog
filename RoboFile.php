9bf19f5c5785618ad778d42390f6b2ffe8c96cb1
coderstephen <me@stephencoakley.com>
2015-07-31 16:18:54 -0500

    Put watch tasks in foreground

--- RoboFile.php
+++ RoboFile.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__.'/vendor/autoload.php';
+<?php require_once __DIR__ . '/vendor/autoload.php';
 
 class RoboFile extends \Robo\Tasks
 {
@@ -11,84 +11,82 @@ class RoboFile extends \Robo\Tasks
     public $ftp_username = 'phing';
     public $ftp_password = 'EtTHuoEMv4pnM48';
 
-    function clean()
+    public function clean()
     {
         $this->say('Cleaning...');
         $this->_cleanDir('cache');
     }
 
-    function fileWatch()
+    public function fileWatch()
     {
         $this->_mkdir('wwwroot/assets/css');
         $this->taskExec('sass')
-            ->arg('--style compressed')
-            ->arg('--no-cache')
-            ->arg('--trace')
-            ->arg('--watch')
-            ->arg($this->scss_input . ':' . $this->scss_output)
-            ->background()
-            ->run();
+             ->arg('--style compressed')
+             ->arg('--no-cache')
+             ->arg('--trace')
+             ->arg('--watch')
+             ->arg($this->scss_input . ':' . $this->scss_output)
+             ->run();
     }
 
-    function build()
+    public function build()
     {
         $this->clean();
         $this->say('Compiling SCSS to CSS...');
 
         $this->_mkdir('wwwroot/assets/css');
         $this->taskExec('sass')
-            ->arg('--style compressed')
-            ->arg('--no-cache')
-            ->arg('--trace')
-            ->arg($this->scss_input)
-            ->arg($this->scss_output)
-            ->run();
+             ->arg('--style compressed')
+             ->arg('--no-cache')
+             ->arg('--trace')
+             ->arg($this->scss_input)
+             ->arg($this->scss_output)
+             ->run();
     }
 
-    function deploy()
+    public function deploy()
     {
         $this->build();
 
         $ftp = $this->taskFtpDeploy($this->ftp_host, $this->ftp_username, $this->ftp_password)
-            ->dir('/')
-            ->from('.')
-            ->exclude('styles')
-            ->exclude('tests')
-            ->secure(false)
-            ->skipSizeEqual()
-            ->skipUnmodified()
-            ->run();
+                    ->dir('/')
+                    ->from('.')
+                    ->exclude('styles')
+                    ->exclude('tests')
+                    ->secure(false)
+                    ->skipSizeEqual()
+                    ->skipUnmodified()
+                    ->run();
     }
 
-    function deployCommit()
+    public function deployCommit()
     {
         $this->build();
 
         // get the files changed in the last commit
         $this->say('Writing last commit diff...');
         $result = $this->taskExec('git')
-            ->arg('diff-tree')
-            ->arg('--no-commit-id')
-            ->arg('--name-only')
-            ->arg('--diff-filter=ACMRT')
-            ->arg('-r master')
-            ->run();
+                       ->arg('diff-tree')
+                       ->arg('--no-commit-id')
+                       ->arg('--name-only')
+                       ->arg('--diff-filter=ACMRT')
+                       ->arg('-r master')
+                       ->run();
         $files = preg_split('/[\r\n]+/', trim($result->getMessage()));
 
         // deploy the changed files
         $this->say('Deploying build diff for last commit...');
         $ftp = $this->taskFtpDeploy($this->ftp_host, $this->ftp_username, $this->ftp_password)
-            ->dir('/test')
-            ->files($files)
-            ->secure(false)
-            ->run();
+                    ->dir('/test')
+                    ->files($files)
+                    ->secure(false)
+                    ->run();
     }
 
-    function serverStart()
+    public function serverStart()
     {
         $this->taskServer(8080)
-            ->dir('wwwroot')
-            ->background()
-            ->run();
+             ->dir('wwwroot')
+             ->run();
     }
 }
