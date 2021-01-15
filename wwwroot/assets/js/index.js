518b7a38aa221fa5e6ff3631b86e39f0914cece5
Stephen M. Coakley <me@stephencoakley.com>
2021-01-14 21:35:34 -0600

    Redesign for 2021 (#74)
    Another website redesign, a refinement on that retro feeling the last few iterations have been after, but this time more subtle and refined.
    
    This isn't fully complete, but I'm OK with designing it out in the open and making incremental changes. This should be good to go as-is.

--- wwwroot/assets/js/index.js
+++ wwwroot/assets/js/index.js
@@ -21,3 +21,40 @@ document.querySelectorAll("pre > code.language-mermaid").forEach(function (code)
 addEventListener('load', function () {
     hljs.initHighlighting();
 }, false);
+
+function animateTyping(element) {
+    let fastSpeed = 40;
+    let slowSpeed = 150;
+    let fullText = element.textContent;
+
+    if (fullText) {
+        // Create a clone of this element and hide the original element. We do
+        // this to preserve the full text of the original element in the DOM to
+        // remain screen-reader friendly instead of changing the document
+        // content repeatedly.
+        let animatedElement = element.cloneNode();
+        animatedElement.setAttribute("aria-hidden", "true");
+
+        let staticElement = element.cloneNode();
+        staticElement.style.display = "none";
+
+        let containerElement = document.createElement("div");
+        containerElement.appendChild(staticElement);
+        containerElement.appendChild(animatedElement);
+
+        element.parentElement.replaceChild(containerElement, element);
+
+        animatedElement.textContent = "";
+        let i = 0;
+
+        setTimeout(function increment() {
+            animatedElement.textContent += fullText[i++];
+
+            if (i < fullText.length) {
+                setTimeout(increment, fullText[i] === " " ? slowSpeed : fastSpeed);
+            }
+        }, fastSpeed);
+    }
+}
+
+document.querySelectorAll("h1").forEach(animateTyping);
